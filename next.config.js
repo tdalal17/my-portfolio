838f66565c4c3fd69f890b816150fb4d205147b8
tdalal17 <89410493+tdalal17@users.noreply.github.com>
2025-03-10 17:43:38 -0500

    Update Next.js config for static export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['images.unsplash.com'],
   },
+  // Ensure basePath is set if you're not using a custom domain
+  basePath: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
 }
 
 module.exports = nextConfig
