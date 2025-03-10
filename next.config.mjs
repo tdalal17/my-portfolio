dc6d5004ab257669469ed3e8b5e014c7657e3080
tdalal17 <89410493+tdalal17@users.noreply.github.com>
2025-03-10 16:45:20 -0500

    Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+}
+
+export default nextConfig;
