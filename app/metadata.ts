c31fefc4874ed185b75ac9a8b1fea4ab8954e7a8
tdalal17 <89410493+tdalal17@users.noreply.github.com>
2025-03-24 02:22:35 -0500

    Create separate metadata file to fix build error

---
+++ app/metadata.ts
@@ -0,0 +1,21 @@
+import { Metadata } from 'next/types'
+
+export const metadata: Metadata = {
+  title: "Tanay Dalal | Software Engineer & Cloud Enthusiast",
+  description: "Personal portfolio of Tanay Dalal, Software Engineer and Cloud Enthusiast",
+  generator: 'v0.dev',
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/favicon-light.ico",
+        href: "/favicon-light.ico",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicon-dark.ico",
+        href: "/favicon-dark.ico",
+      },
+    ],
+  },
+};
